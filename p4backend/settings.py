621604592855c743c38a8c7ed2c57dcb4af8d80b
seolacarrano <seola6@gmail.com>
2020-09-12 19:20:25 -0400

    env

--- p4backend/settings.py
+++ p4backend/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import django_heroku
-#import env as env
+import env as env
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
